97e97f12ffd727ecdb3495126ad6232c10756e25
ascde <ascdee1234@naver,com>
2025-04-27 01:39:02 +0900

    deploy test commit

--- src/main/java/com/smartcart/SmartCartApplication.java
+++ src/main/java/com/smartcart/SmartCartApplication.java
@@ -2,12 +2,18 @@ package com.smartcart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SmartCartApplication {
+public class SmartCartApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartCartApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SmartCartApplication.class);
+	}
 }
